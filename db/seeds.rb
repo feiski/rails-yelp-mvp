787e3f88a4a2c78241684abf02e43bc9117a9a29
Fréederic Baudet <baudet_f@yahoo.fr>
2023-06-14 12:04:06 +0200

    Reviews DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+noodle = {name: "Noodlez", address: "7 Boundary St, London E2 7JE", category: 'chinese'}
+pizza =  {name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", category: 'italian'}
+sushi =  {name: "Sushi Me", address: "56A Shoreditch High St, London E1 6PQ", category: 'japanese'}
+cassoulet =  {name: "Cassolette", address: "56A Shoreditch High St, London E1 6PQ", category: 'french'}
+gauffre =  {name: "C'te gauffre", address: "56A Shoreditch High St, London E1 6PQ", category: 'belgian'}
+
+[noodle, pizza, sushi, cassoulet, gauffre].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
